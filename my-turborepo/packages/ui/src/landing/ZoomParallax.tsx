efa625ed87b8c5a59bc11e8812d12b84dd7b7e38
Rory Teehan <teehanrh@gmail.com>
2025-09-28 14:40:51 -0600

    refactor: simplify zoom parallax by using mapped components and dynamic scaling

--- my-turborepo/packages/ui/src/landing/ZoomParallax.tsx
+++ my-turborepo/packages/ui/src/landing/ZoomParallax.tsx
@@ -11,45 +11,49 @@ export const ZoomParallax = ({ images }: { images: { src: string; alt: string }[
     offset: ['start start', 'end end'],
   });
 
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
-  const y1 = useTransform(scrollYProgress, [0, 1], [0, -300]);
-  const y2 = useTransform(scrollYProgress, [0, 1], [0, -500]);
-  const y3 = useTransform(scrollYProgress, [0, 1], [0, -200]);
-  const y4 = useTransform(scrollYProgress, [0, 1], [0, -450]);
+  const scale4 = useTransform(scrollYProgress, [0, 1], [1, 4]);
+  const scale5 = useTransform(scrollYProgress, [0, 1], [1, 5]);
+  const scale6 = useTransform(scrollYProgress, [0, 1], [1, 6]);
+  const scale8 = useTransform(scrollYProgress, [0, 1], [1, 8]);
+  const scale9 = useTransform(scrollYProgress, [0, 1], [1, 9]);
+
+  const scales = [scale4, scale5, scale6, scale5, scale6, scale8, scale9];
 
   if (images.length < 7) {
-    return null; // Ensure we have enough images
+    return null; // Or a fallback UI
   }
-
-  const [img1, img2, img3, img4, img5, img6, img7] = images;
 
   return (
     <div ref={container} className="relative h-[300vh] bg-black">
       <div className="sticky top-0 h-screen overflow-hidden">
-        {/* Main center image */}
-        <motion.div style={{ scale }} className="absolute inset-0 w-full h-full">
-          <Image src={img1!.src} fill alt={img1!.alt} className="object-cover" />
-        </motion.div>
+        {images.slice(0, 7).map((image, index) => {
+          const scale = scales[index]!;
+          const positionClasses = [
+            '', // Main image, centered by default
+            '[&>div]:!-top-[30vh] [&>div]:!left-[5vw] [&>div]:!h-[30vh] [&>div]:!w-[35vw]', // Image 2
+            '[&>div]:!-top-[10vh] [&>div]:!-left-[25vw] [&>div]:!h-[45vh] [&>div]:!w-[20vw]', // Image 3
+            '[&>div]:!left-[27.5vw] [&>div]:!h-[25vh] [&>div]:!w-[25vw]', // Image 4
+            '[&>div]:!top-[27.5vh] [&>div]:!left-[5vw] [&>div]:!h-[25vh] [&>div]:!w-[20vw]', // Image 5
+            '[&>div]:!top-[27.5vh] [&>div]:!-left-[22.5vw] [&>div]:!h-[25vh] [&>div]:!w-[30vw]', // Image 6
+            '[&>div]:!top-[22.5vh] [&>div]:!left-[25vw] [&>div]:!h-[15vh] [&>div]:!w-[15vw]', // Image 7
+          ];
 
-        {/* Other images with different positions and transforms */}
-        <motion.div style={{ y: y1 }} className="absolute top-[10vh] left-[5vw] w-[20vw] h-[30vh]">
-          <Image src={img3!.src} fill alt={img3!.alt} className="object-cover" />
-        </motion.div>
-        <motion.div style={{ y: y2 }} className="absolute bottom-[10vh] right-[5vw] w-[15vw] h-[25vh]">
-          <Image src={img4!.src} fill alt={img4!.alt} className="object-cover" />
-        </motion.div>
-        <motion.div style={{ y: y3 }} className="absolute top-[5vh] right-[10vw] w-[25vw] h-[20vh]">
-          <Image src={img2!.src} fill alt={img2!.alt} className="object-cover" />
-        </motion.div>
-        <motion.div style={{ y: y4 }} className="absolute bottom-[20vh] left-[10vw] w-[15vw] h-[40vh]">
-          <Image src={img5!.src} fill alt={img5!.alt} className="object-cover" />
-        </motion.div>
-        <motion.div style={{ y: y1 }} className="absolute bottom-[5vh] left-[30vw] w-[10vw] h-[15vh]">
-          <Image src={img6!.src} fill alt={img6!.alt} className="object-cover" />
-        </motion.div>
-        <motion.div style={{ y: y2 }} className="absolute top-[30vh] right-[25vw] w-[12vw] h-[18vh]">
-          <Image src={img7!.src} fill alt={img7!.alt} className="object-cover" />
-        </motion.div>
+          return (
+            <motion.div
+              key={image.src}
+              style={{ scale }}
+              className={`absolute top-0 flex h-full w-full items-center justify-center ${positionClasses[index]}`}>
+              <div className="relative h-[25vh] w-[25vw]">
+                <Image
+                  src={image.src}
+                  fill
+                  alt={image.alt}
+                  className="h-full w-full object-cover"
+                />
+              </div>
+            </motion.div>
+          );
+        })}
       </div>
     </div>
   );
